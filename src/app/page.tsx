739b24b035f5295b44099db92fb96e8795cee325
Nishant <nishantsingh95340@gmail.com>
2024-01-31 12:03:16 +0530

    hero section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { ArrowDownToLine } from "lucide-react";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -9,6 +9,19 @@ const perks = [
   {
     name: "instant Delivery",
     icon: ArrowDownToLine,
+    description: "get your assets on your email in second download them right away",
+  },
+  {
+    name: "Guranted Quality",
+    icon: CheckCircle,
+    description:
+      "every assets on your platform verified by my team i ensure  its our highest quality product . if you are not happy 30 days replacement guranntee with this project",
+  },
+  {
+    name: "For the planet",
+    icon: Leaf,
+    description:
+      "we have pladge 1% of  sales to the preservetion and restoration of the natural enviornment .",
   },
 ];
 
@@ -35,7 +48,24 @@ export default function Home() {
       </MaxWidthWrapper>
       <section className="border-t border-gray-300 bg-gray-100">
         <MaxWidthWrapper className="py-20">
-          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 gap-x-8 lg:gap-y-0"></div>
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perk.icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base text-gray-900 font-medium">{perk.name}</h3>
+                  <p className="mt-3 text-sm text-muted-foreground">{perk.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </MaxWidthWrapper>
       </section>
     </>
