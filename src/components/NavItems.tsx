8b1e13e6682afd8252fcf96c13554b88afcedcc5
Nishant <nishantsingh95340@gmail.com>
2024-02-01 16:26:09 +0530

    add navitems

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,37 @@
-"use client"
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  return <div className="flex gap-4 h-full"></div>;
+  const [active, setActive] = useState<null | number>(null);
+  const isAnyOpen = active !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (active === i) {
+            setActive(null);
+          } else {
+            setActive(i);
+          }
+        };
+
+        const isOpen = i === active;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editior Picks",
+        href: "#",
+        imgSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrival",
+        href: "#",
+        imgSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imgSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favourite Icons Picks",
+        href: "#",
+        imgSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrival",
+        href: "#",
+        imgSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Best selling icons",
+        href: "#",
+        imgSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
